b15abf4949e0df90ef45fa1aea7e46fdd6089021
Nicolas Gonzalez V <ngonza27@eafit.edu.co>
2019-09-13 15:13:17 -0500

    Java Functional-2 Done, some String-1

---
+++ Java/Functional-2/Code.java
@@ -0,0 +1,63 @@
+/*
+* Author: Nicolas Gonzalez Vallejo
+*/
+
+import java.util.List;
+
+class Code {
+
+    public List<Integer> noNeg(List<Integer> nums) {
+        nums.removeIf(x -> x < 0);
+        return nums;
+    }
+
+
+    public List<Integer> no9(List<Integer> nums) {
+        nums.removeIf(n -> n%10 == 9);
+        return nums;
+    }  
+
+    public List<Integer> noTeen(List<Integer> nums) {
+        nums.removeIf(n -> (n >= 13 && n <= 19));
+        return nums;
+    }
+
+
+    public List<String> noZ(List<String> strings) {
+        strings.removeIf(n -> n.contains("z"));
+        return strings;
+    }
+
+    
+    public List<String> noLong(List<String> strings) {
+        strings.removeIf(n -> n.length() > 3);
+        return strings;
+    }
+      
+
+    public List<String> no34(List<String> strings) {
+        strings.removeIf(n -> (n.length()==3 || n.length()==4));
+        return strings;
+    }
+      
+
+    public List<String> noYY(List<String> strings) {
+        strings.replaceAll(n -> n+"y");
+        strings.removeIf(n -> n.contains("yy"));
+        return strings;
+    }
+      
+
+    public List<Integer> two2(List<Integer> nums) {
+        nums.replaceAll(n -> n*2);
+        nums.removeIf(n -> n%10 == 2);
+        return nums;
+    }
+      
+
+    public List<Integer> square56(List<Integer> nums) {
+        nums.replaceAll(n -> ((n*n)+10));
+        nums.removeIf(n -> (n%10==5  || n%10==6));
+        return nums;
+    }      
+}
--- Java/String-1/Code.java
+++ Java/String-1/Code.java
@@ -3,10 +3,30 @@
 */
 
 class Code {
+
     public String helloName(String name) {
         return("Hello "+name+"!");
     }
 
     
+    public String makeAbba(String a, String b) {
+        return a+b+b+a;
+    }
+      
 
+    public String makeTags(String tag, String word) {
+        return "<"+tag+">"+word+"</"+tag+">";
+    }
+
+      
+    public String makeOutWord(String out, String word) {
+        return out.substring(0,2)+word+out.substring(2,4);
+    }
+
+    
+    public String extraEnd(String str) {
+        String a = str.substring(str.length()-2, str.length());
+        return a+a+a;
+    }
+      
 }
