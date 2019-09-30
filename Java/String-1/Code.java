3e87f1b1371cc043dc26ea5ac291979ebe8ba447
ngonza27 <39801147+ngonza27@users.noreply.github.com>
2019-09-30 16:40:01 -0500

    Adding few problems solutions

--- Java/String-1/Code.java
+++ Java/String-1/Code.java
@@ -29,4 +29,60 @@ class Code {
         return a+a+a;
     }
       
+    public String firstTwo(String str) {
+        if (str.length() > 2) 
+          return str.substring(0,2);
+        return str;
+    }
+
+
+    public String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
+    }
+
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
+
+
+    public String comboString(String a, String b) {
+        if (a.length() < b.length()) 
+          return a+b+a;
+        return b+a+b;
+    }
+
+
+    public String nonStart(String a, String b) {
+        return a.substring(1)+b.substring(1);
+    }
+
+
+    public String left2(String str) {
+        return str.substring(2)+str.substring(0,2);
+    }
+      
+    
+    public String right2(String str) {
+        return str.substring(str.length()-2,str.length()) +
+               str.substring(0,str.length()-2);
+    }
+      
+    
+    public String theEnd(String str, boolean front) {
+        if(front)
+          return str.substring(0,1);
+        return str.substring(str.length()-1,str.length());
+    }
+
+
+    public String withouEnd2(String str) {
+        return (str.length() < 2) ?  "" : str.substring(1,str.length()-1);
+    }
+    
+    
+    public String middleTwo(String str) {
+        return str.substring(str.length()/2-1,str.length()/2+1);
+    }
+      
 }
