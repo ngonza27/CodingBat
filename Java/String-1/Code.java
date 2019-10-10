e2e2a0d9ed2f4f877421a88e6051433cb9d95621
ngonza27 <gonzalezvallejonicolas@gmail.com>
2019-10-10 08:02:59 -0500

    Java String-1 completed

--- Java/String-1/Code.java
+++ Java/String-1/Code.java
@@ -147,4 +147,90 @@ class Code {
                str.charAt(str.length()-1) +
                str.charAt(str.length()-2);
     }
+
+
+    public String seeColor(String str) {
+      if (str.length() <= 2) return "";
+      if(str.substring(0,3).equals("red"))
+        return "red";
+      if(str.length() >= 4 && str.substring(0,4).equals("blue"))
+        return "blue";
+      return "";
+    }
+
+
+    public boolean frontAgain(String str) {
+      return str.length() < 2 ? false :
+             str.substring(0,2).equals(str.substring(str.length()-2,str.length()));
+    }
+    
+
+    public String minCat(String a, String b) {
+      if(a.length() > b.length())
+        a = a.substring(a.length()-b.length(), a.length());
+      if(b.length() > a.length())
+        b = b.substring(b.length()-a.length(), b.length());
+      return a+b;
+    }
+    
+
+    public String extraFront(String str) {
+      if(str.length() < 2)
+        return str+str+str; 
+      String sub  = str.substring(0,2);
+      return sub+sub+sub;
+    }
+    
+
+    public String without2(String str) {
+      if(str.length() <= 1)
+        return str;
+      if(str.length() <= 0)
+        return "";
+      return str.substring(0,2).equals(str.substring(str.length()-2,str.length())) ?
+             str.substring(2,str.length()) : str;
+    }
+
+
+    public String deFront(String str) {    
+      if(str.charAt(1) != 'b')
+        str = str.substring(0,1)+str.substring(2,str.length());
+      if(str.charAt(0) != 'a')
+        str = str.substring(1,str.length());
+      return str;
+    }
+    
+
+    public String startWord(String str, String word) {
+      if(word.length() > str.length())
+        return "";
+      str = str.substring(0,word.length());
+      if(word.substring(1,word.length()).equals(str.substring(1,word.length())))
+        return str.substring(0,word.length());
+      return "";
+    }
+
+
+    public String withoutX(String str) {
+      if(str.length() < 2)
+        return "";
+      if(str.substring(0,1).equals("x"))
+        str = str.substring(1,str.length());
+      if(str.substring(str.length()-1,str.length()).equals("x"))
+        str = str.substring(0,str.length()-1);
+      return str;
+    }
+    
+    
+    public String withoutX2(String str) {
+      if(str.length() < 2)
+        return "";
+      if(str.substring(0,2).equals("xx"))
+        return str.substring(2,str.length());
+      if(str.charAt(1) == 'x')
+        return str.substring(0,1)+str.substring(2,str.length());
+      if(str.charAt(0) == 'x')
+        return str.substring(1,str.length());
+      return str;
+    }
 }
