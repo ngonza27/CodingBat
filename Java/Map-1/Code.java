0b6b3a9f439b7f93ee86dad7fcb46514e99d129f
ngonza27 <gonzalezvallejonicolas@gmail.com>
2019-10-10 13:02:48 -0500

    Java Map-1 completed

--- Java/Map-1/Code.java
+++ Java/Map-1/Code.java
@@ -36,5 +36,58 @@ class Code {
         }
         map.put("bread","butter");
         return map;
-    }  
+    } 
+
+
+    public Map<String, String> topping2(Map<String, String> map) {
+      if(map.containsKey("ice cream"))
+        map.put("yogurt",map.get("ice cream"));
+      if(map.containsKey("spinach"))
+        map.put("spinach", "nuts");
+      return map;
+    }
+
+
+    public Map<String, String> topping3(Map<String, String> map) {
+      if(map.containsKey("potato"))
+        map.put("fries", map.get("potato"));
+      if(map.containsKey("salad"))
+        map.put("spinach", map.get("salad"));
+      return map;
+    }
+
+
+    public Map<String, String> mapAB2(Map<String, String> map) {
+      if(map.containsKey("a") && map.containsKey("b") && (map.get("a").equals(map.get("b")))){
+        map.remove("a");
+        map.remove("b");
+      }
+      return map;
+    }
+
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+      if(map.containsKey("a") && !map.containsKey("b"))
+        map.put("b",map.get("a"));
+      if(map.containsKey("b") && !map.containsKey("a"))
+        map.put("a",map.get("b"));
+      return map;
+    }
+
+    
+    public Map<String, String> mapAB4(Map<String, String> map) {
+      if(map.containsKey("a") && map.containsKey("b")){
+        if(map.get("a").length() == map.get("b").length()){
+          map.put("a","");
+          map.put("b","");
+        }
+        if(map.get("a").length() > map.get("b").length()){
+          map.put("c",map.get("a"));
+        } else if(map.get("a").length() < map.get("b").length()){
+          map.put("c",map.get("b"));
+        }
+      }
+      return map;
+    }
+    
 }
