9d4e3853888c0b6cd037d7c3433156f2e867634e
ngonza27 <gonzalezvallejonicolas@gmail.com>
2019-10-12 19:27:14 -0500

    Python String-1 completed

--- Python/String-1/Code.py
+++ Python/String-1/Code.py
@@ -4,3 +4,45 @@ Author: Nicolas Gonzalez Vallejo
 
 def hello_name(name):
   return "Hello "+name+"!"
+
+
+def make_abba(a, b):
+  return a+b+b+a
+
+
+def make_tags(tag, word):
+  return '<'+tag+'>'+word+'</'+tag+'>'
+
+
+def make_out_word(out, word):
+  return out[:2]+word+out[2:]
+
+
+def extra_end(str):
+  return str[-2:]+str[-2:]+str[-2:]
+
+
+def first_two(str):
+  if len(str) < 2:
+    return str
+  return str[:2]
+
+
+def first_half(str):
+  return str[:len(str)/2]
+
+
+def without_end(str):
+  return str[1:len(str)-1]
+
+
+def combo_string(a, b):
+  return b+a+b if len(a) > len(b) else a+b+a
+
+
+def non_start(a, b):
+  return a[1:]+b[1:]
+
+
+def left2(str):
+  return str[2:]+str[:2]
